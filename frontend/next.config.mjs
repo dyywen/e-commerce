290cf79d572b3b7bb05bc86a64d4cd282041d209
Wendy Gilles <wendygilles@hotmail.com>
2025-02-25 17:32:34 +0100

    feat: Login component created and onChange/onCklick added

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,4 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+export default {
+  reactStrictMode: true,
+  // autres options de configuration
+};
--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,9 +1,7 @@
-export default function Home() {
-    return (
-      <div className="min-h-screen bg-gray-100 flex items-center justify-center">
-        <h1 className="text-4xl font-bold text-blue-500">
-          Hello, Tailwind CSS with Next.js!
-        </h1>
-      </div>
-    );
-  }
+import Login from '../components/Login';
+
+function Index() {
+  return <Login />;
+}
+
+export default Index;
